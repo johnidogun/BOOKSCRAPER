1955048526bee89639ca9c937b99fcea1cdf3b71
johnidogun <arsenalidogun@gmail.com>
2025-01-24 08:52:35 +0100

    First Commit

---
+++ main.py
@@ -0,0 +1,54 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+
+current_page =1
+
+
+data = []
+
+
+proceed = True
+ 
+ 
+while(proceed):
+     print("Currently Scraping Page: "+str(current_page))
+    
+     BASE_URL = "https://books.toscrape.com/catalogue/page-"+str(current_page)+".html"
+
+     page = requests.get(BASE_URL)
+
+
+     soup = BeautifulSoup(page.text,"html.parser")
+ 
+     if soup.title.text == "404 Not Found":
+         proceed = False
+     else:  
+         all_books = soup.find_all("li",class_="col-xs-6 col-sm-4 col-md-3 col-lg-3")
+        
+         for book in all_books:
+            item = {}
+            
+            item['Title'] = book.find("img").attrs['alt']
+            
+            item['Link'] = "https://books.toscrape.com/catalogue/"+book.find("a").attrs["href"]
+            
+            item["Price"] = book.find("p", class_="price_color").text[2:]
+            
+            item["Stock"] = book.find("p", class_="instock availability").text.strip()
+            
+            # print(item['Price'])
+            
+            data.append(item)
+        
+     current_page += 1  
+     
+     
+     
+     
+     
+
+df = pd.DataFrame(data)
+df.to_excel("bookss.xlsx")
+df.to_csv("bookss.csv")
